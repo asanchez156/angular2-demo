fa4cb17669a2f5a2a7559e32da2996bf6f43e7fe
Andoni Sánchez <andoni.sanchez.antolin@gmail.com>
2017-04-22 12:23:58 +0200

    added git ignore and angular 2 app

---
+++ app/hello_world_app.component.ts
@@ -0,0 +1,13 @@
+import {Component, View} from "angular2/core";
+
+@Component({
+   selector: 'my-hello'
+})
+
+@View({
+  template: '<h3>Hello World !!</h3>'
+})
+
+export class MyHelloWorldClass {
+
+}
